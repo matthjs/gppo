Corrected old log prob calculation for DSPPs

src/agents/gppoagent.py
@@ -91,24 +91,27 @@ class GPPOAgent(OnPolicyAgent):
             self,
             observation: np.ndarray
     ) -> Union[int, np.ndarray]:
-        state = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0)
-        action_dist, value_dist = self.policy(state)
-        # NOTE: Do something smarter here with action selection
-        action = action_dist.sample()
+        self.policy.eval()
+        with torch.no_grad():
+            state = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0)
+            action_dist, value_dist = self.policy(state)
+            # action_dist = self.policy.policy_likelihood(action_dist)
+            # value_dist = self.policy.value_likelihood(value_dist)
+            action = action_dist.sample().mean(0)
 
-        # log_prob calculation (specific to DSPPs)
-        base_log_marginal = self.policy.policy_likelihood.log_marginal(action, action_dist)
-        deep_log_marginal = self.policy.quad_weights.unsqueeze(-1) + base_log_marginal
-        log_prob = deep_log_marginal.logsumexp(dim=0) # action_dist.log_prob(action)  # NOTE TO SELF THIS IS PROBABLY NOT CORRECT
+            # log_prob calculation (specific to DSPPs)
+            base_log_marginal = self.policy.policy_likelihood.log_marginal(action, action_dist)
+            deep_log_marginal = self.policy.quad_weights.unsqueeze(-1) + base_log_marginal
+            log_prob = deep_log_marginal.logsumexp(dim=0)
 
-        self.last_log_prob = log_prob
-        self.last_value = value_dist.sample().mean(0) if self.sample_vf else value_dist.mean.mean(0)   # value_dist.mean.mean(0)   mean or sample?
+            self.last_log_prob = log_prob
+            self.last_value = value_dist.sample().mean(0) if self.sample_vf else value_dist.mean.mean(0)
 
-        action_t = action.mean(0)
-        if action_t.dim() > 1:
-            return action_t.squeeze(0).cpu().numpy()
+            action_t = action # .mean(0)
+            if action_t.dim() > 1:
+                return action_t.squeeze(0).cpu().numpy()
 
-        return action_t.cpu().numpy()
+            return action_t.cpu().numpy()
 
     def store_transition(
             self,
@@ -133,6 +136,7 @@ class GPPOAgent(OnPolicyAgent):
         Usage: run the learn() method at every timestep in the environment,
         it will only update the agent once the rollout-buffer has been filled.
         """
+        self.policy.train()
         if len(self.rollout_buffer) < self.batch_size:
             return {}
 
@@ -140,6 +144,7 @@ class GPPOAgent(OnPolicyAgent):
         last_state = torch.tensor(self.next_state, dtype=torch.float32, device=self.device).unsqueeze(0)
         with torch.no_grad():
             _, value_dist = self.policy(last_state)
+            # value_dist = self.policy.value_likelihood(value_dist)
             last_value = value_dist.sample().mean(0) if self.sample_vf else value_dist.mean.mean(0)
 
         # compute returns and advantages

src/agents/ppoagent.py
@@ -11,7 +11,7 @@ import torch
 import torch.nn as nn
 from torch.distributions import Normal, Categorical
 from typing import Dict, Any, Tuple, Optional, List
-
+from torch.nn import functional as F
 
 class ActorCritic(nn.Module):
     def __init__(
@@ -123,13 +123,15 @@ class PPOAgent(OnPolicyAgent):
         self.next_state = None
 
     def choose_action(self, observation: np.ndarray) -> Any:
-        state = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0)
-        dist, value, _ = self.policy(state)
-        action = dist.sample()
-        log_prob = dist.log_prob(action).sum(dim=-1)
-        self.last_log_prob = log_prob
-        self.last_value = value
-        return action.cpu().numpy().squeeze(0)
+        self.policy.eval()
+        with torch.no_grad():
+            state = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0)
+            dist, value, _ = self.policy(state)
+            action = dist.sample()
+            log_prob = dist.log_prob(action).sum(dim=-1)
+            self.last_log_prob = log_prob
+            self.last_value = value
+            return action.cpu().numpy().squeeze(0)
 
     def store_transition(
             self,
@@ -169,6 +171,7 @@ class PPOAgent(OnPolicyAgent):
         Usage: run the learn() method at every timestep in the environment,
         it will only update the agent once the rollout_buffer has been filled.
         """
+        self.policy.train()
         # Get last state value for GAE
         if len(self.rollout_buffer) < self.memory_size:
             return {}
@@ -213,7 +216,7 @@ class PPOAgent(OnPolicyAgent):
                     )
 
                 # Value loss --> TD(gae_lambda) target.
-                value_loss = nn.HuberLoss(delta=1)(returns, value_pred.unsqueeze(-1))
+                value_loss = F.mse_loss(returns, value_pred.unsqueeze(-1))
 
                 # Full surrogate loss: L_clip - c1 * L_VF + c_2 S[pi](s_t)
                 loss = policy_loss + self.vf_coef * value_loss - self.ent_coef * entropy

src/gp/acdeepsigma.py
@@ -109,7 +109,7 @@ class ActorCriticDGP(DSPPModel):
         with torch.no_grad():
             policy_dist, value_dist = self(X, mean_input=X)
             if apply_likelihood:
-                policy_dist = self.likelihood(policy_dist)
-                value_dist = self.likelihood(value_dist)
+                policy_dist = self.policy_likelihood(policy_dist)
+                value_dist = self.value_likelihood(value_dist)
 
         return policy_dist, value_dist

src/gp/deepsigma.py
@@ -9,7 +9,7 @@ from gpytorch.likelihoods import GaussianLikelihood, SoftmaxLikelihood
 from gpytorch.models.deep_gps.dspp import DSPP
 from pandas import Categorical
 from torch import Tensor
-
+import torch.nn as nn
 from src.gp.deepgplayers import DSPPHiddenLayer
 from src.gp.gpbase import GPytorchModel
 
@@ -53,16 +53,21 @@ class DSPPModel(DSPP, GPytorchModel):
             hidden_layers_config
 
         # Build hidden layers
-        for layer_config in hidden_layers_config:
-            hidden_layer = DSPPHiddenLayer(
-                input_dims=input_dims,
-                output_dims=layer_config['output_dims'],
-                num_inducing=num_inducing_points,
-                mean_type=layer_config['mean_type'],
-                Q=Q
-            )
-            self.layers.append(hidden_layer)
-            input_dims = hidden_layer.output_dims if hidden_layer.output_dims else 1
+        if hidden_layers_config is None:
+            print("[DSPP Constructor] WARNING: No hidden layers configuration is provided. Forward()"
+                  " will call the identity function.")
+            self.layers.append(nn.Identity())
+        else:
+            for layer_config in hidden_layers_config:
+                hidden_layer = DSPPHiddenLayer(
+                    input_dims=input_dims,
+                    output_dims=layer_config['output_dims'],
+                    num_inducing=num_inducing_points,
+                    mean_type=layer_config['mean_type'],
+                    Q=Q
+                )
+                self.layers.append(hidden_layer)
+                input_dims = hidden_layer.output_dims if hidden_layer.output_dims else 1
 
         self.out_dim = input_dims
         self.layers = torch.nn.ModuleList(self.layers)